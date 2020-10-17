3e2eeb47625d9c504960c9893e38da1b51e80cc3
Caleb Adepitan <calebpitan@outlook.com>
2020-10-17 05:38:41 +0100

    User query resolver

---
+++ app/graphql/resolver/query/user.ts
@@ -0,0 +1,18 @@
+import { UserModel } from '../../../model'
+import { Context } from '../../types'
+import { Filter, Select, UserFilterInput, UserSelectFieldInput } from '../types'
+
+class UserQueryResolver {
+  static async user(root: any, args: Filter<UserFilterInput>, context: Context) {
+    const user = await UserModel.findOne(args.filter)
+    return user
+  }
+
+  static async currentUser(root: any, args: Select<UserSelectFieldInput>, context: Context) {
+    return {
+      username: 'caleb',
+    }
+  }
+}
+
+export default UserQueryResolver
