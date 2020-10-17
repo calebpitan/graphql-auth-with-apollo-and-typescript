e003b10265a1a6664f4f9218d88d9bb79188a462
Caleb Adepitan <calebpitan@outlook.com>
2020-10-17 05:40:41 +0100

    All type defs

---
+++ app/graphql/typedefs/index.ts
@@ -0,0 +1,8 @@
+import User from './user'
+import Input from './input'
+import Mutation from './mutation'
+import Query from './query'
+
+const typeDefs = [Input, User, Query, Mutation]
+
+export default typeDefs
