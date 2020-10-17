53bada67d7b5cff15304d63ee70a389f8f41dfb2
Caleb Adepitan <calebpitan@outlook.com>
2020-10-17 05:38:58 +0100

    Index query resolvers

---
+++ app/graphql/resolver/query/index.ts
@@ -0,0 +1,10 @@
+import { Context } from '../../types'
+import { QueryType } from '../types'
+import UserResolver from './user'
+
+const Query: QueryType = {
+  user: UserResolver.user,
+  currentUser: UserResolver.currentUser,
+}
+
+export default Query
