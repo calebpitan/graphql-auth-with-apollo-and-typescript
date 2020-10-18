b46a77f57d1e6e013eff189f348566539442014a
Caleb Adepitan <calebpitan@outlook.com>
2020-10-18 15:04:35 +0100

    Basic utility functions

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -3,3 +3,18 @@ import { Express } from 'express'
 export function isProduction(app: Express) {
   return app.get('env') === 'production'
 }
+
+export function createBadInput<TInput extends { [k: string]: any }>(input: TInput) {
+  return { input }
+}
+
+export function createSession<TData extends { [k: string]: any }>(
+  req: Express.Request,
+  data: TData
+) {
+  if (!req.session) {
+    return null
+  }
+
+  return Object.assign(req.session, data)
+}
