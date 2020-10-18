a0c7f3e2ba0da2221dd4f42985fd2f279b699350
Caleb Adepitan <calebpitan@outlook.com>
2020-10-18 15:04:14 +0100

    Add a token expiry time; set to 1 day

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,5 +1,6 @@
 export const SALT_ROUNDS = 8
 export const SECRET = process.env.SECRET
+export const CLIENT_TOKEN_EXPIRY = 3600 * 24 // 1 day in seconds
 
 export const CORS = {
   credentials: true,
