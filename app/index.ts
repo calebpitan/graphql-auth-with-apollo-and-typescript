65ee5071c26a0ff3e50b816a2cf3681accaa3ab9
Caleb Adepitan <calebpitan@outlook.com>
2020-10-18 14:52:45 +0100

    Update
    * Modify mongoose connection options to silence deprecation warnings
    
    * Modify error event handler
    
    * Instantiate MongoStore from connect-mong and use with express-session
    
    * Move the express-session middleware HOF out to a variable
    
    * Create a custom middleware to parse authorization header and add an
    auth object to the request
    
    * Set 'trust proxy' in production

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import mongoose from 'mongoose'
+import session from 'express-session'
+import mongostore from 'connect-mongo'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
@@ -9,16 +11,22 @@ import { v4 as uuidV4 } from 'uuid'
 
 import typeDefs from './graphql/typedefs'
 import resolvers from './graphql/resolver'
-import { CORS } from './utils/constants'
+import { CORS, SECRET } from './utils/constants'
+import { isProduction } from './utils'
+import { verifyToken } from './utils/auth'
 
 mongoose
   .connect(process.env.MONGO_CONNECT_STRING as string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    // findOneAndUpdate should be used by mongoose instead of findOneAndModify
+    // https://mongoosejs.com/docs/deprecations.html#findandmodify
+    useFindAndModify: false,
   })
   .catch(err => console.error(err))
 
-mongoose.connection.on('error', err => console.error('Internal mongoose error: ', err))
+mongoose.connection.on('error', console.error.bind(console, 'Internal mongoose error: '))
 
 const app = express()
 
@@ -30,14 +38,45 @@ const server = new ApolloServer({
   },
 })
 
-app.set('trust proxy', 1)
+const MongoStore = mongostore(session)
+
+const sessionMiddleware = session({
+  genid: () => uuidV4(),
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  secret: SECRET as string, // It will fail anyway if undefined
+  saveUninitialized: false,
+  unset: 'keep',
+  resave: false,
+  proxy: isProduction(app),
+  cookie: {
+    httpOnly: true,
+    secure: isProduction(app),
+    sameSite: 'strict',
+  },
+})
 
 // app.use(helmet())
 app.use(cors(CORS))
-
+app.use(sessionMiddleware)
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function (req, _res, next) {
+  const { authorization } = req.headers
+  const nobearer = [, undefined]
+  const [, bearer] =
+    typeof authorization === 'string' ? /bearer\s+(.+)/i.exec(authorization) ?? nobearer : nobearer
+
+  verifyToken(bearer as string)
+    .then(token => (req.auth = { client: token }))
+    .catch(() => console.log(bearer)) // Intentionally pass on error
+  next()
+})
+
+if (isProduction(app)) {
+  app.set('trust proxy', 1)
+}
 
 server.applyMiddleware({ app, path: '/graphql', cors: CORS })
 
