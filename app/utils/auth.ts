3abf2d12fb76044195f8fa76cddae6e4b50b1689
Caleb Adepitan <calebpitan@outlook.com>
2020-10-18 15:03:45 +0100

    Utility functions for auth purpose

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,5 +1,10 @@
-import { SALT_ROUNDS } from './constants'
+import { ApolloError } from 'apollo-server-express'
 import bcrypt from 'bcrypt'
+import jwt, { SignOptions, Secret } from 'jsonwebtoken'
+import { JWTExpiredError } from '../errors'
+
+import { SALT_ROUNDS, SECRET } from './constants'
+import { Pluck, ClientJWTPayload, ServerJWTPayload, VerifiedAuthPayload } from './types'
 
 export async function hashPasword(password: string) {
   const salt = await bcrypt.genSalt(SALT_ROUNDS)
@@ -10,4 +15,30 @@ export async function hashPasword(password: string) {
 export async function comparePassword(password: string, hashed: string) {
   const similar = await bcrypt.compare(password, hashed)
   return similar
+}
+
+export async function createToken(data: any, expiry: Pluck<SignOptions, 'expiresIn'>) {
+  return await Promise.resolve(jwt.sign(data, SECRET as Secret, { expiresIn: expiry }))
+}
+
+export async function verifyToken(token: string): Promise<VerifiedAuthPayload> {
+  try {
+    return (await Promise.resolve(jwt.verify(token, SECRET as Secret))) as VerifiedAuthPayload
+  } catch (e) {
+    const { name, message, ...extensions } = e
+    if (name === 'TokenExpiredError') {
+      throw new JWTExpiredError(message, extensions)
+    }
+    throw new ApolloError(message, name)
+  }
+}
+
+export function payloadFromUser({
+  _id,
+  account_id,
+  email,
+}: ServerJWTPayload): [ClientJWTPayload, ServerJWTPayload] {
+  const client = { _id, account_id }
+  const server = { _id, account_id, email }
+  return [client, server]
 }
